2a9866cbb02889deb1971d8e9d6ebed6b683dae2
ehsanjourah <ehsanjourah@gmail.com>
2024-10-01 13:08:20 +0100

    design implemented

---
+++ resources/views/home.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.main')
+@section('content')
+    <div class="hero">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7">
+                    <div class="intro-wrap">
+                        <h1 class="mb-5"><span class="d-block">Calculate your commuting </span> carbon <span class="typed-words"></span></h1>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <form class="form">
+                                    <div class="row mb-2">
+                                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                                            <select name="" id="" class="form-control custom-select">
+                                                <option value="">Destination</option>
+                                                <option value="">Peru</option>
+                                                <option value="">Japan</option>
+                                                <option value="">Thailand</option>
+                                                <option value="">Brazil</option>
+                                                <option value="">United States</option>
+                                                <option value="">Israel</option>
+                                                <option value="">China</option>
+                                                <option value="">Russia</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-5">
+                                            <input type="text" class="form-control" name="daterange">
+                                        </div>
+                                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-3">
+                                            <input type="text" class="form-control" placeholder="# of People">
+                                        </div>
+
+                                    </div>    
+                                    <div class="row align-items-center">
+                                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                                            <input type="submit" class="btn btn-primary btn-block" value="Search">
+                                        </div>
+                                        {{-- <div class="col-lg-8">
+                                            <label class="control control--checkbox mt-3">
+                                                <span class="caption">Save this search</span>
+                                                <input type="checkbox" checked="checked" />
+                                                <div class="control__indicator"></div>
+                                            </label>
+                                        </div> --}}
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="slides">
+                        <img src="{{ asset('images/hero-slider-1.jpg') }}" alt="Image" class="img-fluid active">
+                        <img src="{{ asset('images/hero-slider-2.jpg') }}" alt="Image" class="img-fluid">
+                        <img src="{{ asset('images/hero-slider-3.jpg') }}" alt="Image" class="img-fluid">
+                        <img src="{{ asset('images/hero-slider-4.jpg') }}" alt="Image" class="img-fluid">
+                        <img src="{{ asset('images/hero-slider-5.jpg') }}" alt="Image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="untree_co-section count-numbers py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-6 col-sm-6 col-md-6 col-lg-4">
+					<div class="counter-wrap">
+						<div class="counter">
+							<span class="" data-number="9313">0</span>
+						</div>
+						<span class="caption">No. of Vehicles</span>
+					</div>
+				</div>
+				<div class="col-6 col-sm-6 col-md-6 col-lg-4">
+					<div class="counter-wrap">
+						<div class="counter">
+							<span class="" data-number="8492">0</span>
+						</div>
+						<span class="caption">No. of People</span>
+					</div>
+				</div>
+				<div class="col-6 col-sm-6 col-md-6 col-lg-4">
+					<div class="counter-wrap">
+						<div class="counter">
+							<span class="" data-number="100">0</span>
+						</div>
+						<span class="caption">Quantity of Carbon Emissions</span>
+					</div>
+				</div>
+				{{-- <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+					<div class="counter-wrap">
+						<div class="counter">
+							<span class="" data-number="120">0</span>
+						</div>
+						<span class="caption">No. of Countries</span>
+					</div>
+				</div> --}}
+			</div>
+		</div>
+	</div>
+@stop
+
+@section('script')
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,125 @@
+    
+        <div class="site-footer">
+            <div class="inner first">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-4">
+                            <div class="widget">
+                                <h3 class="heading">About Green Travel</h3>
+                                <p>Calculate to measure your commute's impact on the environment.</p>
+                            </div>
+                            <div class="widget">
+                                <ul class="list-unstyled social">
+                                    <li><a href="#"><span class="icon-twitter"></span></a></li>
+                                    <li><a href="#"><span class="icon-instagram"></span></a></li>
+                                    <li><a href="#"><span class="icon-facebook"></span></a></li>
+                                    <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                                    <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                                    <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                                    <li><a href="#"><span class="icon-apple"></span></a></li>
+                                    <li><a href="#"><span class="icon-google"></span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-2 pl-lg-5">
+                            <div class="widget">
+                                <h3 class="heading">Pages</h3>
+                                <ul class="links list-unstyled">
+                                    <li><a href="{{ url('/')}}">Home</a></li>
+                                    <li><a href="{{ url('/')}}">Services</a></li>
+                                    <li><a href="{{ url('/')}}">About</a></li>
+                                    <li><a href="{{ url('/')}}">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        {{-- <div class="col-md-6 col-lg-2">
+                            <div class="widget">
+                                <h3 class="heading">Resources</h3>
+                                <ul class="links list-unstyled">
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div> --}}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="widget">
+                                <h3 class="heading">Contact</h3>
+                                <ul class="list-unstyled quick-info links">
+                                    <li class="email"><a href="#">mail@example.com</a></li>
+                                    <li class="phone"><a href="#">+353 22 333 4444</a></li>
+                                    <li class="address"><a href="#">Dublin City University, Collins Ave Ext, Whitehall, Dublin 9</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
+            <div class="inner dark">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col-md-8 mb-3 mb-md-0 mx-auto">
+                            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div id="overlayer"></div>
+        <div class="loader">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        
+        
+        {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> --}}
+        <script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+        <script src="{{ asset('js/popper.min.js') }}"></script>
+        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
+        <script src="{{ asset('js/aos.js') }}"></script>
+        <script src="{{ asset('js/moment.min.js') }}"></script>
+        <script src="{{ asset('js/daterangepicker.js') }}"></script>
+
+        <script src="js/typed.js"></script>
+        <script>
+            $(function() {
+                var slides = $('.slides'),
+                images = slides.find('img');
+
+                images.each(function(i) {
+                    $(this).attr('data-id', i + 1);
+                })
+
+                var typed = new Typed('.typed-words', {
+                    strings: ["footprint."," footprint."],
+                    typeSpeed: 80,
+                    backSpeed: 80,
+                    backDelay: 4000,
+                    startDelay: 1000,
+                    loop: true,
+                    showCursor: true,
+                    preStringTyped: (arrayPos, self) => {
+                        arrayPos++;
+                        console.log(arrayPos);
+                        $('.slides img').removeClass('active');
+                        $('.slides img[data-id="'+arrayPos+'"]').addClass('active');
+                    }
+
+                });
+            })
+        </script>
+
+        <script src="js/custom.js"></script>
+        @yield('script')
+    </body>
+</html>
