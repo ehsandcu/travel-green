e3c5b180983409c111f474c5483f4a21cd7c1d8f
ehsanjourah <ehsanjourah@gmail.com>
2024-10-03 14:36:17 +0100

    login page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,11 +38,15 @@
             <nav class="site-nav">
                 <div class="container">
                     <div class="site-navigation">
-                        <a href="{{ route('home')}}" class="logo m-0 text-decoration-none">Green Travel <span class="text-primary">.</span></a>
-        
+                        <a href="{{ route('home')}}" class="logo m-0 text-decoration-none">
+                            Green Travel 
+                            <span class="text-primary">.</span>
+                        </a>        
                         <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-end">
-                            <li class="active"><a href="{{ route('home') }}">Home</a></li>
-                            <li class="has-children">
+                            <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                                <a href="{{ route('home') }}">Home</a>
+                            </li>
+                            {{-- <li class="has-children">
                                 <a href="#">Dropdown</a>
                                 <ul class="dropdown">
                                     <li><a href="elements.html">Elements</a></li>
@@ -57,11 +61,20 @@
                                     </li>
                                     <li><a href="#">Menu Three</a></li>
                                 </ul>
+                            </li> --}}
+                            <li class="{{ request()->routeIs('service.index') ? 'active' : '' }}">
+                                <a href="{{ route('service.index') }}">Services</a>
                             </li>
-                            <li><a href="{{ route('service.index') }}">Services</a></li>
-                            <li><a href="{{ route('about.us') }}">About</a></li>
-                            <li><a href="{{ route('contact.us') }}">Contact Us</a></li>
-                        </ul>        
+                            <li class="{{ request()->routeIs('about.us') ? 'active' : '' }}">
+                                <a href="{{ route('about.us') }}">About</a>
+                            </li>
+                            <li class="{{ request()->routeIs('contact.us') ? 'active' : '' }}">
+                                <a href="{{ route('contact.us') }}">Contact Us</a>
+                            </li>
+                            <li>   
+                                <a href="{{ route('login')}}" class="btn btn-primary btn-block">Login</a>    
+                            </li>
+                        </ul>  
                         <a href="#" class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light" data-toggle="collapse" data-target="#main-navbar">
                             <span></span>
                         </a>        
