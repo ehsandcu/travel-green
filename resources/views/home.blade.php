c6bdbbc8b633cba9ac8a06251c79d4f8d8b0800f
ehsanjourah <ehsanjourah@gmail.com>
2024-10-08 10:58:32 +0100

    carbon emission value to home page completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,17 @@
             width: 100%;
             height: inherit;
         }
+
+        #carbon-result {
+            font-size: 14px;
+            font-weight: normal;
+            color: #333;
+            margin-bottom: 6px;
+            text-align: center;
+            background-color: #f0f0f0;
+            padding: 10px;
+            border-radius: 30px;
+        }
     </style>
 @stop
 @section('content')
@@ -16,11 +27,11 @@
                         <h1 class="mb-5"><span class="d-block">Calculate your commuting </span> carbon <span class="typed-words"></span></h1>
                         <div class="row">
                             <div class="col-12">
-                                <form class="form">
+                                <form class="form" id="carbon_form">
                                     <div class="row mb-2">
                                         <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                                            <label for="transport-method">Transport Method</label>                                            
-                                            <select id="transport-method" class="form-control custom-select">
+                                            <label for="transport_method">Transport Method</label>                                            
+                                            <select id="transport_method" class="form-control custom-select">
                                                 <option value="0.1645">Petrol Car</option>
                                                 <option value="0.16984">Diesel Car</option>
                                                 <option value="0.0514">Electric Car</option>
@@ -32,25 +43,21 @@
                                               </select>
                                         </div>
                                         <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-5">
-                                            <label for="transport-method">Distance to Work(km)</label>
-                                            <input type="number" class="form-control" name=""  placeholder="Distance to Work">
+                                            <label for="work_distance">Distance to Work(km)</label>
+                                            <input type="number" id="work_distance" class="form-control" min="1" value="1" placeholder="Distance to Work">
                                         </div>
                                         <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-3">                                            
-                                            <label for="transport-method">Work Days per Week</label>
-                                            <input type="text" class="form-control" placeholder="Work Days">
+                                            <label for="work_days">Work Days per Week</label>
+                                            <input type="number" id="work_days" class="form-control" min="1" value="1" placeholder="Work Days">
                                         </div>
                                     </div>    
                                     <div class="row align-items-center">
                                         <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
-                                            <input type="submit" class="btn btn-primary btn-block" value="Calculate">
-                                        </div>
-                                        {{-- <div class="col-lg-8">
-                                            <label class="control control--checkbox mt-3">
-                                                <span class="caption">Save this search</span>
-                                                <input type="checkbox" checked="checked" />
-                                                <div class="control__indicator"></div>
-                                            </label>
-                                        </div> --}}
+                                            <input type="submit" class="btn btn-primary btn-block calculate_carbon" value="Calculate">
+                                        </div>    
+                                        <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                                            <p id="carbon-result">CO2e per year <span id="emission_value"></span></p>                          
+                                        </div>          
                                     </div>
                                 </form>
                             </div>
@@ -110,4 +117,19 @@
 @stop
 
 @section('script')
+    <script>
+        $(document).on('submit', '#carbon_form', function(e){
+            e.preventDefault();
+
+            const transportMethod = $('#transport_method').val();
+            const workDistance = parseFloat($('#work_distance').val());
+            const workDays = parseFloat($('#work_days').val());
+            const weeksPerYear = 48;
+
+            if (workDays > 0 && workDistance > 0 && transportMethod >= 0) {
+                const co2eEmissions = (transportMethod * workDistance * 2 * workDays * weeksPerYear).toFixed(2);
+                $('#emission_value').text(co2eEmissions + 'kg');                
+            }
+        });
+   </script>
 @endsection
