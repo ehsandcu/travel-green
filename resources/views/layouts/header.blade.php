a9818586823545dfad4dca64ba14d33bc4845e2b
ehsanjourah <ehsanjourah@gmail.com>
2024-10-11 12:43:47 +0100

    updated logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
             <meta name="csrf-token" content="{{ csrf_token() }}"/>
             <meta name="author" content="Muhammad Ehsan" />
             <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
-            <title>Travel Green| @yield('title', 'Commuting Carbon Calculator')</title>
+            <title>Travel Green | @yield('title', 'Commuting Carbon Calculator')</title>
 
             {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
 
