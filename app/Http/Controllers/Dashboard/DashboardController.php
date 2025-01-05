9736f3e8319e65739c2b92b049b6e38c1ee71142
ehsanjourah <ehsanjourah@gmail.com>
2025-01-05 15:52:58 +0000

    error checking

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -141,7 +141,7 @@ class DashboardController extends Controller
             }
 
         $campusResult = $campusQuery->groupBy('destination_latlng')->get();
-        
+        dd($campusResult);
         $resultWithCampus = $campusResult->map(function ($item) use ($latLngToCampus) {
             if ($item->destination_latlng) {
                 $item->campus_name = DcuCampus::CAMPUSES[$latLngToCampus[$item->getRawOriginal('destination_latlng')]]['label'] ?? 'Unknown Campus';
