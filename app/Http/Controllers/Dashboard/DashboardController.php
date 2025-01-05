1a332b28efc26b85786e495925a87a73e3dfd2e3
ehsanjourah <ehsanjourah@gmail.com>
2025-01-05 16:23:10 +0000

    minor updated

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -121,7 +121,7 @@ class DashboardController extends Controller
 
         //format like '{"lat":"12.23","lng":"4.56"}' => 'Campus A',
         foreach ($campuses as $campusKey => $campus) {
-           $latLngToCampus['{"lat":'.$campus['latitude'].',"lng":'.$campus['longitude'].'}'] = $campusKey;
+           $latLngToCampus['{"lat":"'.$campus['latitude'].'","lng":"'.$campus['longitude'].'"}'] = $campusKey;
         }
         
         $latLngs = array_keys($latLngToCampus);
@@ -143,13 +143,13 @@ class DashboardController extends Controller
         
         $campusResult = $campusQuery->groupBy('destination_latlng')->get();
         $resultWithCampus = $campusResult->map(function ($item) use ($latLngToCampus) {
-            if (isset($latLngToCampus[$item->getRawOriginal('destination_latlng')])) {
+            if ($item->destination_latlng) {
                 $item->campus_name = DcuCampus::CAMPUSES[$latLngToCampus[$item->getRawOriginal('destination_latlng')]]['label'] ?? 'Unknown Campus';
                 return $item;
             }
         });
         
-        dd($resultWithCampus,$campusResult, $latLngs);
+        dd($resultWithCampus, $latLngs,$campusResult);
        return $this->sendResponse([
             'success' => 1,
             'message' => "Data Listed Successfully.",
